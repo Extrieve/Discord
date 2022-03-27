5ff19a81f1e416a3147b17cd500c0996babe88bb
Nick <nicolas.cinera@gmail.com>
2022-03-27 17:25:25 -0400

    Bug Fixes and Backups

--- cloud_bot.py
+++ cloud_bot.py
@@ -7,34 +7,31 @@ import re
 from discord.ext import commands
 from datetime import date
 from numpy import random
-from dotenv import load_dotenv
-
-# Create environment variables
-load_dotenv()
+from run import keep_alive
 
 
 class MyClient(discord.Client):
 
     ogMemes = ['https://imgur.com/ST9RRdZ', 'https://imgur.com/YOFy6O2', 'https://imgur.com/NHJscWK',
-            'https://imgur.com/AQrKLHS', 'https://imgur.com/T2kmXqQ', 'https://imgur.com/xSrY6F3', 'https://imgur.com/NfFlB1a']
+               'https://imgur.com/AQrKLHS', 'https://imgur.com/T2kmXqQ', 'https://imgur.com/xSrY6F3', 'https://imgur.com/NfFlB1a']
 
     greats = ['https://cdn.discordapp.com/attachments/193188992857014272/859280267352080384/e3012c5d489016f804b00696a7b0427e.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/859163415079813130/Junkiesbeseethinginthecoments_8d0f7dc9b13e41603fa14fb38bc8bb73.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/858198346844864562/Whatarethepurposeofthesecomicsjustcirclejerk_ea8428881b521b3e489f5516fc48f0d9.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/845385715427704862/Depressedrook_49d925_8440415.mp4', 'https://discord.com/channels/193188992857014272/193188992857014272/840789509532024833', 'https://cdn.discordapp.com/attachments/193188992857014272/824357641048162334/1fb7394de568f412c32e28ef6f63f86c.mp4', 'https://cdn.discordapp.com/attachments/595792876873580546/857081275193491517/video0-143.mp4', 'https://cdn.discordapp.com/attachments/787867027775029259/852957245804249099/video0-18.mp4', 'https://cdn.discordapp.com/attachments/386718408982528000/854385108295483442/video0.mov', 'https://cdn.discordapp.com/attachments/564169696547700746/854191093830516786/IM_GOING_TO_KILL_CHAOS.mp4',
-            'https://cdn.discordapp.com/attachments/825060471937040415/833469538108178442/sus.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/860225401703628820/qwe.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/856647303476805673/a_tu_casa.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/850526888421097502/video_1.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/850140189178200094/xdd.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/847901681777377281/getfvid_191184229_503335914218882_4166493292746332674_n.mp4', 'https://cdn.discordapp.com/attachments/715909144267456543/773793957146263582/123289186_364189861337254_4649832349965682829_n.mp4', 'https://cdn.discordapp.com/attachments/729055475445923964/759092722627117056/AngryChineseGuy.mov', 'https://cdn.discordapp.com/attachments/228646713026543618/861732683452776488/mzl2q7QxGDf8qbYDftcg_02_e908eaa7b460ecbf27ba255aca6d7fa8_video.mp4', 'https://2eu.funnyjunk.com/movies/Boom_7648b0_7820344.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/583096390964740106/meandtheboys.mp4']
+              'https://cdn.discordapp.com/attachments/825060471937040415/833469538108178442/sus.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/860225401703628820/qwe.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/856647303476805673/a_tu_casa.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/850526888421097502/video_1.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/850140189178200094/xdd.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/847901681777377281/getfvid_191184229_503335914218882_4166493292746332674_n.mp4', 'https://cdn.discordapp.com/attachments/715909144267456543/773793957146263582/123289186_364189861337254_4649832349965682829_n.mp4', 'https://cdn.discordapp.com/attachments/729055475445923964/759092722627117056/AngryChineseGuy.mov', 'https://cdn.discordapp.com/attachments/228646713026543618/861732683452776488/mzl2q7QxGDf8qbYDftcg_02_e908eaa7b460ecbf27ba255aca6d7fa8_video.mp4', 'https://2eu.funnyjunk.com/movies/Boom_7648b0_7820344.mp4', 'https://cdn.discordapp.com/attachments/193188992857014272/583096390964740106/meandtheboys.mp4']
 
     dota = ['https://cdn.discordapp.com/attachments/862790378925850624/867206248469757982/479019.mp4',
-        'https://cdn.discordapp.com/attachments/222852560422174720/861740384336740363/DOTA_2_RAGE_-_PERUANO_Y_VENEZOLANO_ENOJADOS__.mp4']
+            'https://cdn.discordapp.com/attachments/222852560422174720/861740384336740363/DOTA_2_RAGE_-_PERUANO_Y_VENEZOLANO_ENOJADOS__.mp4']
 
     miercoles = [
-    'https://cdn.discordapp.com/attachments/862790378925850624/867642282203414528/5n5dibawgvd61.png']
+        'https://cdn.discordapp.com/attachments/862790378925850624/867642282203414528/5n5dibawgvd61.png']
 
     jueves = ['https://tenor.com/view/asuka-feliz-jueves-running-happy-thursday-gif-16469364',
-            'https://cdn.discordapp.com/attachments/862790378925850624/867203979996954684/feliz_jueves.mp4']
+              'https://cdn.discordapp.com/attachments/862790378925850624/867203979996954684/feliz_jueves.mp4']
 
     viernes = [
-    'https://cdn.discordapp.com/attachments/193188992857014272/762724813147209788/hapifr.mp4']
+        'https://cdn.discordapp.com/attachments/193188992857014272/762724813147209788/hapifr.mp4']
 
     week = ["Lunes", "Martes", "Miercoles",
-        "Jueves", "Viernes", "Sabado", "Domingo"]
+            "Jueves", "Viernes", "Sabado", "Domingo"]
 
     workingD = os.getcwd()
 
@@ -44,8 +41,8 @@ class MyClient(discord.Client):
     categories = ['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle', 'cry', 'hug', 'awoo', 'kiss', 'lick', 'pat', 'smug', 'bonk', 'yeet', 'blush', 'smile', 'wave', 'highfive', 'handhold', 'nom', 'bite', 'glomp', 'slap', 'kill', 'kick', 'happy', 'wink', 'poke',
                   'dance', 'cringe']
 
-
     # Get anime quote
+
     def getQuote(self):
         # Get Api
         s = requests.Session()
@@ -57,12 +54,11 @@ class MyClient(discord.Client):
         quote = f"Quote: {json_data['quote']}\nCharacter: {json_data['character']}\nSeries: {json_data['anime']}"
         return quote
 
-
     def getDay(self):
         day = str(date.today()).split('-')
         day = [int(x) for x in day]
         week_num = date(day[0], day[1], day[2]).weekday()
-        today = self.week[week_num] 
+        today = self.week[week_num]
 
         if today.lower() == 'miercoles':
             rand = random.randint(0, len(self.miercoles))
@@ -81,13 +77,12 @@ class MyClient(discord.Client):
             f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}')
         processedResponse = json.loads(response.text)
         return f"Definition: {processedResponse[0]['meanings'][0]['definitions'][0]['definition']}"
-    
+
     def getImage(self, search):
         response = requests.get(
             f'https://imsea.herokuapp.com/api/1?q=<{search}>')
         response_json = json.loads(response.text)
         return response_json['results']
-
 
     async def on_ready(self):
         print('Logged in as')
@@ -109,7 +104,7 @@ class MyClient(discord.Client):
 
         if message.content.startswith('$hello'):
             await message.channel.send('Hello!')
-        
+
         if message.content.startswith('$animeQuote'):
             await message.channel.send(self.getQuote())
 
@@ -138,7 +133,7 @@ class MyClient(discord.Client):
                 option = await self.wait_for('message', check=is_correct, timeout=5.0)
             except asyncio.TimeoutError:
                 return await message.channel.send(f'Sorry, you took too long, terminating request...')
-            
+
             if option.content == '1':
                 await message.channel.send(message.author.avatar_url)
             elif option.content == '2':
@@ -153,7 +148,7 @@ class MyClient(discord.Client):
 
             def longEnough(m):
                 return True if len(m.content) > 2 and m.author == message.author else False
-            
+
             try:
                 name = await self.wait_for('message', check=longEnough, timeout=10)
             except asyncio.TimeoutError:
@@ -177,7 +172,8 @@ class MyClient(discord.Client):
             await message.channel.send('\n'.join(reply_list))
 
             def is_correct(m):
-                good_range = True if 0 <= int(m.content) <= len(reply_list) else False
+                good_range = True if 0 <= int(
+                    m.content) <= len(reply_list) else False
                 values = True if m.content.isdigit() else False
                 return m.author == message.author and values and good_range
 
@@ -187,7 +183,8 @@ class MyClient(discord.Client):
                 return await message.channel.send(f'Sorry, you took too long, terminating request...')
 
             final_res = list()
-            final_res.append(self.animeDB['data'][list(matches.keys())[int(option.content) - 1]]['title'])
+            final_res.append(self.animeDB['data'][list(matches.keys())[
+                             int(option.content) - 1]]['title'])
             final_res.append(
                 f"Type: {self.animeDB['data'][list(matches.keys())[int(option.content) - 1]]['type']}")
             final_res.append(
@@ -204,7 +201,7 @@ class MyClient(discord.Client):
 
             def longEnough(m):
                 return True if len(m.content) > 2 and m.author == message.author else False
-            
+
             try:
                 search = await self.wait_for('message', check=longEnough, timeout=10)
                 if len(search.content) <= 2:
@@ -213,7 +210,8 @@ class MyClient(discord.Client):
                 return await message.channel.send(f'Sorry, you took too long, terminating request...')
 
             base_url = 'https://api.jikan.moe/v4/anime'
-            request = requests.get(base_url, params={'q': search.content}, verify=False)
+            request = requests.get(
+                base_url, params={'q': search.content}, verify=False)
 
             if request.status_code == 200:
                 data = json.loads(request.text)
@@ -223,11 +221,11 @@ class MyClient(discord.Client):
 
                 await message.channel.send('Select an option:')
                 await message.channel.send('\n'.join(results))
-                
+
                 def is_correct(m):
                     values = True if m.content.isdigit() else False
                     return m.author == message.author and values
-                
+
                 try:
                     choice = await self.wait_for('message', check=is_correct, timeout=10)
                 except asyncio.TimeoutError:
@@ -235,15 +233,17 @@ class MyClient(discord.Client):
 
                 choice = int(choice.content) - 1
 
-                if choice >= len(data['data']) or choice < 0:
+                if choice > len(data['data']) or choice <= 0:
                     return await message.channel.send('Invalid choice, terminating request...')
 
                 final_res = []
                 try:
                     final_res.append(data['data'][choice]['title'])
                     final_res.append(f"Score: {data['data'][choice]['score']}")
-                    final_res.append(f"Rated: {data['data'][choice]['rating']}")
-                    final_res.append(f"Popularity Ranking: {data['data'][choice]['popularity']}")
+                    final_res.append(
+                        f"Rated: {data['data'][choice]['rating']}")
+                    final_res.append(
+                        f"Popularity Ranking: {data['data'][choice]['popularity']}")
                     final_res.append(
                         f"Studio: {data['data'][choice]['studios'][0]['name']}")
                     final_res.append(data['data'][choice]['synopsis'])
@@ -265,12 +265,13 @@ class MyClient(discord.Client):
                 # await message.channel.send(final_res)
             else:
                 return await message.channel.send('No entries found')
-        
+
         # if users ask for a joke, then reply to them with a joke
         # This is the power of copilot
         if message.content.startswith('$joke'):
             # get joke from api and send it
-            joke = requests.get('https://icanhazdadjoke.com/', headers={"Accept": "application/json"})
+            joke = requests.get('https://icanhazdadjoke.com/',
+                                headers={"Accept": "application/json"})
             await message.channel.send(joke.json()['joke'])
 
         # Copilot did this
@@ -286,12 +287,12 @@ class MyClient(discord.Client):
             # make request verification false so that we can get the answer
             s = requests.Session()
             s.verify = False
-            
+
             # reply to their question with a random fact about cars
             # get a random fact about cars
             fact = s.get('https://catfact.ninja/fact')
             await message.channel.send(f'{question.content} You ask? Well...\n{fact.json()["fact"]}')
-            
+
         # if users ask for an anime picture, then reply to them with a picture
         if message.content.startswith('$animePic'):
             # ask them if they want a picture from a random tag or a specific tag
@@ -302,7 +303,7 @@ class MyClient(discord.Client):
                 option = await self.wait_for('message', check=lambda m: m.author == message.author, timeout=12)
             except asyncio.TimeoutError:
                 return await message.channel.send(f'Sorry, you took too long, terminating request...')
-            
+
             if option.content.lower() == 'random':
                 # get a random tag
                 rand = random.randint(0, len(self.categories))
@@ -322,13 +323,13 @@ class MyClient(discord.Client):
                 # Make sure the user is asking for a tag that exists
                 def is_correct(m):
                     return m.author == message.author and m.content.lower() in self.categories
-                
+
                 try:
                     option = await self.wait_for('message', check=is_correct, timeout=12)
                 except asyncio.TimeoutError:
                     # tell the user their option is not valid
                     return await message.channel.send(f'Sorry, you took too long, terminating request...')
-                
+
                 # get picture from user option
                 response = requests.get(
                     f'https://api.waifu.pics/sfw/{option.content.lower()}').text
@@ -421,14 +422,14 @@ class MyClient(discord.Client):
                                         params={'q': sentence.content}, verify=False)
                     response = req.text.replace('[', '').replace(']', '')
                     lang = json.loads(response)['language']
-                    
+
                     await message.channel.send(f'What is your target language?')
 
                     try:
                         target = await self.wait_for('message', check=lambda m: m.author == message.author, timeout=12)
                     except asyncio.TimeoutError:
                         return await message.channel.send(f'Sorry, you took too long, terminating request...')
-                    
+
                     if target.content.lower() in langs:
                         target = langs[target.content.lower()]
                         req = requests.post('https://libretranslate.de/translate',
@@ -436,14 +437,15 @@ class MyClient(discord.Client):
                         final = json.loads(req)['translatedText']
                         await message.channel.send(final)
 
-                
-            elif word.content == '2':    
+            elif word.content == '2':
                 await message.channel.send(', '.join(langs.keys()))
             else:
                 return await message.channel.send('Sorry, that is not a valid option')
+
 
 intents = discord.Intents.default()
 intents.members = True
 
 client = MyClient()
-client.run(os.getenv('TOKEN'))
+keep_alive()
+client.run(os.environ['token'])
